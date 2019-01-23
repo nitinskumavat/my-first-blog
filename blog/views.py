3a987b8bf9f7bf6d930047dafb6b1744ede24438
Nitin Kumavat <nitin.s.kumavat@gmail.com>
2019-01-23 09:51:01 +0530

    trying to solve error:NoReverseMatch at /

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ def post_list(request):
     return render(request,'blog/post_list.html',{'posts':posts})
 
 def post_detail(request, pk):
-    post=get_object_or_404(Post,pk=pk)
-    return render(request,'blog/post_detail.html',{'post':post})
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
